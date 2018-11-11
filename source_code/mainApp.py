5ba0ec6e5142636818d7a8b182df777937be1ca2
liviubrinza <blf_titi@yahoo.com>
2018-11-11 23:35:41 +0200

    added value sensitive command handling

--- source_code/mainApp.py
+++ source_code/mainApp.py
@@ -1,6 +1,15 @@
 from NeuralNetwork import NeuralNetwork
 from OneHotEncoder import OneHotEncoder
 
+def get_value(sentence):
+    words = sentence.split()
+    for word in words:
+        try:
+            return int(word)
+        except ValueError:
+            continue
+    return None
+
 encoder = OneHotEncoder()
 encoder.load_data()
 net = NeuralNetwork(encoder)
@@ -10,5 +19,14 @@ while True:
     sentence = input("Enter command:")
     if sentence == "quit":
         break
-    sentence = encoder.encode_sentence(sentence)
-    print(encoder.categories[net.classify(sentence)])
+    encoded_sentence = encoder.encode_sentence(sentence)
+    category_no = net.classify(encoded_sentence)
+
+    if category_no == 3 or category_no == 4:
+        value = get_value(sentence)
+        if value:
+            print("\tConverted command: " + encoder.categories[category_no] + " by " + str(value))
+            continue
+    print("\tConverted command: " + encoder.categories[category_no])
+
+
